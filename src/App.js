5a2f36a21877f10b22a8729351fcfca17ea72dcb
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-06-06 04:07:45 +0100

    initial commit webpack not copiling

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>React app</h1>
+        </div>
+    )
+}
+
+export default App;
